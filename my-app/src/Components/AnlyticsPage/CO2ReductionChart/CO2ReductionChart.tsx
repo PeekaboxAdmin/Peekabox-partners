b66c300b70e0bc51dbe42c58cdb3a2fc106557c1
BatoulAls <batoul.alsharif@icloud.com>
2024-11-19 17:33:23 +0400

    Charts For insight Page

---
+++ my-app/src/Components/AnlyticsPage/CO2ReductionChart/CO2ReductionChart.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, ResponsiveContainer, Cell, LabelList } from 'recharts';
+import './CO2ReductionChart.css'
+type DataPoint = {
+  category: string;
+  value: number;
+};
+
+const data: DataPoint[] = [
+  { category: 'Bakery Items', value: 87 },
+  { category: 'Dairy', value: 60 },
+  { category: 'Fruit Bag', value: 110 },
+  { category: 'Meal Bag', value: 75 },
+  { category: 'Bakery Bag', value: 90 },
+  { category: 'Snack Bag', value: 134 },
+  { category: 'Veg Bag', value: 43 }
+];
+
+const CO2ReductionChart: React.FC = () => {
+  return (
+    <div className="chart-container">
+      <div className="chart-header">
+       
+        <div className="chart-left-header">
+          <div className="chart-header-content">
+            <div className="writing-mode-vertical">
+              <h3 className="chart-title">CO₂ Reduction (Kg)</h3>
+            </div>
+          </div>
+        </div>
+
+        {/* Chart Section */}
+        <div className="chart-right-section">
+          <div className="chart-percentage">
+            <span className="percentage-text">▲ +2.45%</span>
+          </div>
+          
+          <ResponsiveContainer width="100%" height={250}>
+            <BarChart
+              data={data}
+              margin={{ top: 20, right: 30, left: 0, bottom: 40 }}
+            >
+              <defs>
+                <linearGradient id="barGradient" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="20%" stopColor="#FF80B4" stopOpacity={0.8} />
+                  <stop offset="80%" stopColor="#0B4B2C" stopOpacity={0.5} />
+                </linearGradient>
+              </defs>
+              
+              <CartesianGrid
+                horizontal={true}
+                vertical={false}
+                stroke="#E5E7EB"
+                strokeDasharray="3 3"
+              />
+              
+              <XAxis
+                dataKey="category"
+                axisLine={false}
+                tickLine={false}
+                tick={{ fill: '#828898', fontSize: 12 ,fontWeight:'600px'}}
+                interval={0}
+                angle={0}
+                textAnchor="middle"
+                dy={20}
+              />
+              
+              <YAxis
+                axisLine={false}
+                tickLine={false}
+                tick={{ fill: '#828898', fontSize: 12 }}
+                domain={[0, 150]}
+                ticks={[0, 50, 100, 150]}
+              />
+              
+              <Bar
+                dataKey="value"
+                radius={[20, 20, 0, 0]}
+                barSize={20}
+              >
+                {data.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill="url(#barGradient)" />
+                ))}
+                
+                <LabelList 
+                  dataKey="value"
+                  position="top" 
+                  fontSize={14}
+                  fill="#A1A9BC" 
+                />
+              </Bar>
+
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export default CO2ReductionChart
---
+++ my-app/src/Components/AnlyticsPage/MonthlySalesChart/MonthlySalesChart.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
+import './MonthlySalesChart.css';
+
+const data = [
+  { month: 'Mar', sales: 150 },
+  { month: 'April', sales: 80 },
+  { month: 'May', sales: 100 },
+  { month: 'Jun', sales: 55 },
+  { month: 'Jul', sales: 180 },
+  { month: 'Aug', sales: 80 },
+  { month: 'Sep', sales: 250 },
+];
+
+const MonthlySalesChart = () => {
+  return (
+    <div className="chart-container">
+      <h2 className="chart-title">Monthly Net Sales</h2>
+      <div className="chart-wrapper">
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            data={data}
+            margin={{ top: 20, right: 30, left: 20, bottom: 20 }}
+            style={{ pointerEvents: 'none' }}
+          >
+            <CartesianGrid
+              horizontal={true}
+              vertical={false}
+              strokeDasharray="3 3"
+              stroke="#82889880"
+            />
+            <XAxis
+              dataKey="month"
+              tick={{ fill: '#666', fontSize: '12px' }}
+              axisLine={false}
+              tickLine={false} 
+              padding={{ left: 30, right: 30 }} 
+            />
+            <YAxis
+              label={{
+                value: 'In (AED)',
+                angle: -90,
+                position: 'insideLeft',
+                style: { textAnchor: 'middle' },
+              }}
+              tick={{ fill: '#666', fontSize: '12px' }}
+              axisLine={false} 
+              tickLine={false} 
+              domain={[0, 250]}
+              ticks={[0, 50, 100, 150, 200, 250]} 
+              interval={0} 
+            />
+            <Tooltip
+              contentStyle={{
+                backgroundColor: 'white',
+                border: '1px solid #ccc',
+                borderRadius: '4px',
+              }}
+            />
+            <Line
+              type="linear"
+              dataKey="sales"
+              stroke="#FF69B4"
+              strokeWidth={2}
+              dot={false}  
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
+};
+
+export default MonthlySalesChart;
