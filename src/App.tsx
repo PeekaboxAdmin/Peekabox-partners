e96cb682a6b9d0f276e06b1f6600cf8c48c9715d
Nasif4567 <ahmedshahriar12@gmail.com>
2025-03-11 02:29:44 +0600

    Revert "Resolved merge conflicts and finalized merge"
    This reverts commit 14b6baa1ead5328b6b86d9ec35e9d793f232e42b.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { io } from 'socket.io-client';
+
 import Sidebar from './Components/Sidebar';
 import Dashboard from './Components/Dashbaord'; 
 import SurpriseBoxManagement from './Components/SurpriseBox';
@@ -24,58 +26,15 @@ import './App.css';
 const AppContent: React.FC = () => {
   const [showNotification, setShowNotification] = useState(false);
 
-// need to more this from here 
-  /*const firebaseConfig = {
-    apiKey: "AIzaSyCeNCZLpi4AVCR1FXg_FiUrGmoNWnxny2A",
-    authDomain: "peekabox-dd3c8.firebaseapp.com",
-    projectId: "peekabox-dd3c8",
-    storageBucket: "peekabox-dd3c8.firebasestorage.app",
-    messagingSenderId: "984679613645",
-    appId: "1:984679613645:web:f3664d523876562ddd1584",
-    measurementId: "G-W7LR5YBK6M"
-  };
+  useEffect(() => {
+    socket.on('newOrderNotification', () => {
+      setShowNotification(true);
+    });
 
-  // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const messaging = getMessaging(firebaseApp);
-
-
-useEffect(() => {
-  // Request permission for notifications
-  const requestNotificationPermission = async () => {
-    try {
-      const token = await getToken(messaging, {
-        vapidKey: 'your-vapid-key', // You can get this from Firebase Console
-      });
-      if (token) {
-        console.log('FCM Token:', token);
-
-        // Send token to your backend to associate it with the user or store it
-        // Example: sendTokenToBackend(token);
-      } else {
-        console.log('No registration token available');
-      }
-    } catch (error) {
-      console.error('Error getting FCM token:', error);
-    }
-  };
-
-  // Listen for incoming messages while the app is in the foreground
-  onMessage(messaging, (payload) => {
-    console.log('Message received: ', payload);
-    // Handle the notification and display it
-    //alert('New Notification: ' + payload.notification.title);
-  });
-
-  requestNotificationPermission();
-
-  // Cleanup on unmount
-  return () => {
-    // Cleanup Firebase listeners, if necessary
-  };
-}, []);
-
-*/
+    return () => {
+      socket.off('newOrderNotification');
+    };
+  }, []);
 
   return (
     <div className="App">
@@ -93,6 +52,8 @@ useEffect(() => {
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
 
+      {/* Display OrderNotification when a new order is received */}
+      {showNotification && <OrderNotification />}
     </div>
   );
 };
